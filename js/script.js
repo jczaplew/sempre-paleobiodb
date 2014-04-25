ef177022bb2571140fe7feb17768ebe68a309e0e
John J Czaplewski <jczaplew@gmail.com>
2014-04-25 15:33:24 -0500

    Added bones for returning user feedback

--- js/script.js
+++ js/script.js
@@ -16,6 +16,36 @@
       this.form.on('submit', $.proxy(this.search, this));
     },
 
+    rebindEvents: function() {
+      $(".resultLink").on("click", function(e) {
+        var index = $(e.target).attr("id").replace("link", ""),
+            currentSearch = $("[name=search]").val();
+
+        // let sempre know it's been clicked
+      /*
+        $.getJSON("http://chaconne.stanford.edu:8500/sempre?q=" + currentSearch + "&accept=" + index + "&format=json", function(done) {
+          // done
+        });
+      */
+      });
+      // Handler for best result checkboxes
+      $("input[type=checkbox]").on("change", function(e) {
+        $("input[type=checkbox").prop("checked", false);
+
+        var checked = $(e.target),
+            best = checked.val();
+
+        checked.prop("checked", true);
+        
+      /*
+        // let sempre know it's been clicked
+        $.getJSON("http://chaconne.stanford.edu:8500/sempre?q=" + currentSearch + "&accept=" + best + "&format=json", function(done) {
+          // done
+        });
+      */  
+      });
+    },
+
     search: function(event) {
       event.preventDefault();
 
@@ -23,12 +53,17 @@
 
       $.getJSON("http://chaconne.stanford.edu:8500/sempre?q=" + query.replace(/ /g, "+") + "&format=json", function(results) {
 
-        results.candidates.forEach(function(d) {
+        results.candidates.forEach(function(d, i) {
           d.stars = search.getStars(d.score);
+
+          // Mustache can't access the index, so we have to do this
+          d.index = i;
         });
 
         var output = Mustache.render(search.template, results);
         $("#results").html(output);
+
+        search.rebindEvents();
 
         return false;
       });
